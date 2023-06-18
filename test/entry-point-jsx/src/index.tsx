532a8d71a063e1c2c248dd9251b0fcedb050cd80
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2023-06-18 12:08:35 +0200

    update jsx entry point

--- test/entry-point-jsx/src/index.tsx
+++ test/entry-point-jsx/src/index.tsx
@@ -40,83 +40,73 @@ export type TTile = CustomElement<HTMLElement , ITile>
 
 export let Tile = (options:{name:string}) => {
 
+    let setTileTime = (target:TTile , time:Date) => {
+
+        let nbr1 = target.children['nbr1'] as HTMLSpanElement;
+        let nbr2 = target.children['nbr2'] as HTMLSpanElement;
+
+        time = new Date(time);
+
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let secondes = time.getSeconds();
+
+        if(options.name == 'heures'){
+
+            let [timeNbr1 , timeNbr2] = String(hours).padStart(1,'0').split('');
+
+            if(timeNbr1 && nbr1.getAttribute('value') != timeNbr1){
+                ThoriumAnimation.RotateDown( nbr1 );
+                nbr1.setAttribute('value' , timeNbr1);
+                nbr1.innerText = timeNbr1;
+            }
+            if(timeNbr2 && nbr2.getAttribute('value') != timeNbr2){
+                ThoriumAnimation.RotateDown( nbr2 );
+                nbr2.setAttribute('value' , timeNbr2);
+                nbr2.innerText = timeNbr2;
+            }
+
+        }
+        if(options.name == 'minutes'){
+
+            let [timeNbr1 , timeNbr2] = String(minutes).padStart(1,'0').split('');
+
+            if(timeNbr1 && nbr1.getAttribute('value') != timeNbr1){
+                ThoriumAnimation.RotateDown( nbr1 );
+                nbr1.setAttribute('value' , timeNbr1);
+                nbr1.innerText = timeNbr1;
+            }
+            if(timeNbr2 && nbr2.getAttribute('value') != timeNbr2){
+                ThoriumAnimation.RotateDown( nbr2 );
+                nbr2.setAttribute('value' , timeNbr2);
+                nbr2.innerText = timeNbr2;
+            }
+
+        }
+        if(options.name == 'secondes'){
+
+            let [timeNbr1 , timeNbr2] = String(secondes).padStart(1,'0').split('');
+
+            if(timeNbr1 && nbr1.getAttribute('value') != timeNbr1){
+                ThoriumAnimation.RotateDown( nbr1 );
+                nbr1.setAttribute('value' , timeNbr1);
+                nbr1.innerText = timeNbr1;
+            }
+            if(timeNbr2 && nbr2.getAttribute('value') != timeNbr2){
+                ThoriumAnimation.RotateDown( nbr2 );
+                nbr2.setAttribute('value' , timeNbr2);
+                nbr2.innerText = timeNbr2;
+            }
+
+        }
+    }
+
     return <div 
         class = {Style.Tile}
         name = {options.name}
-        _setTime = {(target:TTile , time:Date) => {
-
-            let nbr1 = target.children['nbr1'] as HTMLSpanElement;
-            let nbr2 = target.children['nbr2'] as HTMLSpanElement;
-
-            time = new Date(time);
-
-            let hours = time.getHours();
-            let minutes = time.getMinutes();
-            let secondes = time.getSeconds();
-
-            if(options.name == 'heures'){
-
-                let [timeNbr1 , timeNbr2] = String(hours).padStart(1,'0').split('');
-
-                if(timeNbr1 && nbr1.getAttribute('value') != timeNbr1){
-                    ThoriumAnimation.RotateDown( nbr1 );
-                    nbr1.setAttribute('value' , timeNbr1);
-                    nbr1.innerText = timeNbr1;
-                }
-                if(timeNbr2 && nbr2.getAttribute('value') != timeNbr2){
-                    ThoriumAnimation.RotateDown( nbr2 );
-                    nbr2.setAttribute('value' , timeNbr2);
-                    nbr2.innerText = timeNbr2;
-                }
-
-            }
-            if(options.name == 'minutes'){
-
-                let [timeNbr1 , timeNbr2] = String(minutes).padStart(1,'0').split('');
-
-                if(timeNbr1 && nbr1.getAttribute('value') != timeNbr1){
-                    ThoriumAnimation.RotateDown( nbr1 );
-                    nbr1.setAttribute('value' , timeNbr1);
-                    nbr1.innerText = timeNbr1;
-                }
-                if(timeNbr2 && nbr2.getAttribute('value') != timeNbr2){
-                    ThoriumAnimation.RotateDown( nbr2 );
-                    nbr2.setAttribute('value' , timeNbr2);
-                    nbr2.innerText = timeNbr2;
-                }
-
-            }
-            if(options.name == 'secondes'){
-
-                let [timeNbr1 , timeNbr2] = String(secondes).padStart(1,'0').split('');
-
-                if(timeNbr1 && nbr1.getAttribute('value') != timeNbr1){
-                    ThoriumAnimation.RotateDown( nbr1 );
-                    nbr1.setAttribute('value' , timeNbr1);
-                    nbr1.innerText = timeNbr1;
-                }
-                if(timeNbr2 && nbr2.getAttribute('value') != timeNbr2){
-                    ThoriumAnimation.RotateDown( nbr2 );
-                    nbr2.setAttribute('value' , timeNbr2);
-                    nbr2.innerText = timeNbr2;
-                }
-
-            }
-        }}
-    >
-        <span
-            name = {"nbr1"}
-            value = {0}
-            text = {'0'}
-        />
-        <span
-            name = {"nbr2"}
-            value = {0}
-            text = {'0'}
-            _onmutation = {(mutation) => {
-                console.log(mutation)
-            }}
-        />
+        _setTime = {setTileTime}>
+            <span name = {"nbr1"} value = {0} text = {'0'}/>
+            <span name = {"nbr2"} value = {0} text = {'0'}/>
     </div>
 }
 
@@ -135,21 +125,22 @@ export let Clock = () => {
         if(clock)clock.updateTime( clock , Date.now())
     } , 1000)
 
+    let updateTime = (target:TClock , time:number) => {
+        let heures = target.children['heures'];
+        let minutes = target.children['minutes'];
+        let secondes = target.children['secondes'];
+        heures.setTime( heures , time );
+        minutes.setTime( minutes , time );
+        secondes.setTime( secondes , time );
+    }
+
     return <div
         id = {clokId}
         class = {Style.Clock}
-        _updateTime = {(target:TClock , time:number) => {
-            let heures = target.children['heures'];
-            let minutes = target.children['minutes'];
-            let secondes = target.children['secondes'];
-            heures.setTime( heures , time );
-            minutes.setTime( minutes , time );
-            secondes.setTime( secondes , time );
-        }}
-    >
-        <Tile name={"heures"}/>
-        <Tile name={"minutes"}/>
-        <Tile name={"secondes"}/>
+        _updateTime = {updateTime}>
+            <Tile name={"heures"}/>
+            <Tile name={"minutes"}/>
+            <Tile name={"secondes"}/>
     </div>;
 
 }
